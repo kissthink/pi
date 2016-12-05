08fc82c29bcf4266a9b839f490f9c9a9a568f4cc
mike <mike@zeoalliance.com>
2016-12-05 15:18:18 +0200

    basic auth implementation

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
-import "github.com/surgemq/surgemq/service"
+import (
+	"github.com/surgemq/surgemq/service"
+	"github.com/surgemq/surgemq/auth"
+	"errors"
+)
+
+const login = "foo"
+const pass = "bar"
+
+type MyAuth struct {}
+
+func (m MyAuth) Authenticate(id string, cred interface{}) error {
+
+	if id == login && cred.(string) == pass {
+		return nil
+	}
+
+	return errors.New("Wrong cred")
+}
 
 func main() {
-	svr := &service.Server{}
+	m := MyAuth{}
+	provider := auth.Authenticator(m)
+	auth.Register("my", provider)
+
+	svr := &service.Server{
+		Authenticator: "my",
+	}
 
 	svr.ListenAndServe("tcp://:1883")
 }
